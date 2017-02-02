939ba566f5f5f318bc1d5b70e1b091d34e6111f7
EJMason <eliotjunior@gmail.com>
2017-02-02 15:05:24 -0800

    [feat] redux actions added

---
+++ app/src/actions/loginActions.js
@@ -0,0 +1,30 @@
+
+export function setName(name) {
+  return {
+    type: 'SET_USER_NAME',
+    payload: name
+  }
+}
+
+export function setUserId(id) {
+  return {
+    type: 'SET_USER_ID',
+    payload: id
+  }
+}
+
+export function userLogin(data) {
+  return {
+    type: 'USER_LOGIN_SUCCESS',
+    payload: data
+  }
+}
+
+export function mountApp() {
+  return {
+    type: 'APP_MOUNTED',
+    payload: true
+  }
+}
+
+
--- app/src/actions/userActions.js
+++
@@ -1,7 +0,0 @@
-
-export function setName(name) {
-  return {
-    type: 'SET_USER',
-    payload: name
-  }
-}
