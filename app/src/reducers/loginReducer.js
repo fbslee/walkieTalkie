ef5bab46a00ca199e04ddf7accc7d18e1bda8fe1
EJMason <eliotjunior@gmail.com>
2017-02-02 15:04:49 -0800

    [feat] reducers for login functionality

--- app/src/reducers/loginReducer.js
+++ app/src/reducers/loginReducer.js
@@ -1,13 +1,37 @@
 const defaultProps = {
   userId: null,
-  name: null
+  name: null,
+  logged_in: false,
+  mounted: false,
 }
 
 export default function reducer(state = defaultProps, action) {
   switch (action.type) {
 
-    case 'SET_USER': {
-      return {...state, userID: action.payload}
+    case 'SET_USER_NAME': {
+      return {...state, 
+        name: action.payload
+      }
+      break
+    }
+    case 'SET_USER_ID': {
+      return {...state, 
+        userID: action.payload
+      }
+      break
+    }
+    case 'USER_LOGIN_SUCCESS': {
+      return {...state, 
+        userId: action.payload.userId,
+        name: action.payload.name,
+        logged_in: action.payload.logged_in
+      }
+      break
+    }
+    case 'APP_MOUNTED': {
+      return {...props,
+        mounted: action.payload  
+      }
     }
   }
 
