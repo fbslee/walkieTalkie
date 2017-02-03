a035a4dd1ff8e70ebe1c9bb6d9b342ce8ca21854
EJMason <eliotjunior@gmail.com>
2017-02-02 23:13:39 -0800

    [feat] redux additions to actions chat and nav

--- app/src/actions/chatActions.js
+++ app/src/actions/chatActions.js
@@ -1,6 +1,23 @@
+
 export function changeRoom(room) {
   return {
-    type: 'CHANGE_ROOM',
-    payload: room
-  }
+    type: 'CHANGE_CHAT_ROOM',
+    payload: room,
+  };
+}
+
+export function chatExit() {
+  return {
+    type: 'CHAT_EXIT',
+    payload: false,
+  };
+}
+
+export function chatSelection(inputRoomId, searchOptions, result) {
+  return {
+    type: 'CHAT_SELECT',
+    payload: {
+      inputRoomId, searchOptions, result,
+    },
+  };
 }
