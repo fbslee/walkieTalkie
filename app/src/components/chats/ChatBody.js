b02e6dd1ada34457f2c633d142033ef4e15a77e1
EJMason <eliotjunior@gmail.com>
2017-02-03 01:42:19 -0800

    [refactor] minor changes

--- app/src/components/chats/ChatBody.js
+++ app/src/components/chats/ChatBody.js
@@ -1,44 +1,34 @@
-import React, {Component} from 'react'
-import {connect} from 'react-redux'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-import Chatroom from './chatroom/Chatroom'
-import ChatSelection from './ChatSelection'
+import Chatroom from './chatroom/Chatroom';
+import ChatSelection from './ChatSelection';
 
-import {chatExit, changeRoom, chatSelection} from '../../actions/chatActions'
+import { chatExit, changeRoom, chatSelection } from '../../actions/chatActions';
 
-@connect(store => {
-  return {
-    roomId: store.chat.roomId,
-    chat_view: store.chat.chat_view,
-    roomSearch: store.chat.roomSearch,
-    searchResults: store.chat.roomSearch,
-    logged_in: store.login.logged_in
-  }
-})
+@connect(store => ({
+  roomId: store.chat.roomId,
+  chat_view: store.chat.chat_view,
+  roomSearch: store.chat.roomSearch,
+  searchResults: store.chat.roomSearch,
+  logged_in: store.login.logged_in,
+}))
 class ChatBody extends Component {
 
- handleRoomChange(newRoom) {
-  this.props.dispatch(changeRoom())
-  //  this.setState({
-  //    roomId : newRoom,
-  //  })
- }
+  vhandleRoomChange(newRoom) {
+    this.props.dispatch(changeRoom(newRoom));
+  }
 
-  handleChatSelection(inputRoomId, searchOptions, result){
-    this.props.dispatch(ChatSelection(inputRoomId, searchOptions, result))
-  //  this.setState({
-  //    roomId : inputRoomId,
-  //    roomSearch : {'option' : searchOptions, 'res' : result},
-  //    chat_view : true
-  //  })
- }
+  handleChatSelection(inputRoomId, searchOptions, result) {
+    this.props.dispatch(ChatSelection(inputRoomId, searchOptions, result));
+  }
 
   render() {
     return (
       <div>
         {
-          (this.props.chat_view) 
-          ? <Chatroom /> 
+          (this.props.chat_view)
+          ? <Chatroom />
           : <ChatSelection selectRoom={::this.handleChatSelection} />
         }
       </div>
@@ -46,5 +36,5 @@ class ChatBody extends Component {
   }
 }
 
-export default ChatBody
+export default ChatBody;
 
