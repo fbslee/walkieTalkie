66967316ae0e92e6b67f577bf93de07b49b00731
EJMason <eliotjunior@gmail.com>
2017-02-03 21:24:25 -0800

    [refactor] cleaned App file

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -1,58 +1,25 @@
-import React, { PropTypes, Component } from 'react';
-import { Router } from 'react-router';
-import axios from 'axios'
-import {connect} from 'react-redux'
+import React, { Component } from 'react';
+import axios from 'axios';
+import {connect} from 'react-redux';
 import Auth0Lock from 'auth0-lock';
-import AuthService from '../utils/AuthService'
+import AuthService from '../utils/AuthService';
 
+import Chat from './chatComponents/Chat';
 import ChatBody from './chats/ChatBody';
 import ViewNavBar from './topBar/ViewNavbar';
-import keys from '../../../keys'
-import {mountApp, userLogin, userLogout, authLogin} from '../actions/loginActions'
-import { chatExit } from '../actions/chatActions';
+import keys from '../../../keys';
 
 import './App.css';
 
-import { chatExit } from '../actions/chatActions';
-
-//====================================
- import Chat from './chatComponents/Chat';
-//===================================
-
-import './App.css';
+ 
 
 @connect(store => ({
 
 }))
 class App extends Component {
 
-  // componentWillMount() {
-  //   axios.get('/checkSession').then((res) => {
-  //     if (res.data.id) {
-  //       console.log('AXIOS: WE HAVE AN ID D: !');
-  //       axios.post('/exitChat', { id: this.props.userId }).then((response) => {
-  //         console.log(response);
-  //       });
-  //       axios.post('/logout', { id: this.props.userId }).then((response) => {
-  //         console.log(response);
-  //       });
-  //       console.log('AXIOS: GOT SESSION ID!');
-  //     // this.props.dispatch(userLogin(res))
-  //     } else {
-  //       console.log('AXIOS: NO ID D: !');
-  //       this.props.dispatch(mountApp());
-  //     }
-  //   }).catch((err) => {
-  //     console.log(err);
-  //   });
-  // }
-
-  componentWillMount(){
-    this.lock = new AuthService(keys.keys.AUTH0_CLIENT_ID, keys.keys.AUTH0_DOMAIN)
-  }
-
-  handleUserAuthLogin(res) {
-    this.props.dispatch(authLogin(res));
+  componentWillMount() {
+    this.lock = new AuthService(keys.keys.AUTH0_CLIENT_ID, keys.keys.AUTH0_DOMAIN);
   }
 
   Logged() {
@@ -61,15 +28,15 @@ class App extends Component {
       console.log(getProfile)
   }
 
-
-
   render() {
-    const token = this.lock.loggedIn()
-    
-    // ( this.lock.loggedIn() )
-    // ? <ChatBody onload={Logged()}/> 
-    // : <ChatBody onload={notLogged()}/>
-
+    console.log(this.lock.loggedIn())
+    //return <ChatBody /> 
+    //const token = this.lock.loggedIn()
+    if(this.lock.loggedIn()) {
+      return <ChatBody /> 
+    } else {
+      <ChatBody onload={this.lock.login()}/>
+    }
   }
 }
 
